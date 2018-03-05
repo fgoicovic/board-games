c8b2208d33871b34fa8dad95160625fa0bef6f3a
Felipe G. Goicovic <fagarri1@uc.cl>
2018-03-05 13:09:33 +0100

    Small bugfix in istambul

--- istanbul.py
+++ istanbul.py
@@ -17,7 +17,7 @@ def print_opts():
 def exchange_tiles(board, pos1, pos2):
     aux = board[pos1]
     board[pos1] = board[pos2]
-    board[pos2] = aux 
+    board[pos2] = aux
 
 
 def deploy_random_board(game):
@@ -33,14 +33,14 @@ def deploy_random_board(game):
         print('Invalid option %d.'%(game))
         print_opts()
         exit()
-    
+
     ntiles = rows * columns
     board = zeros((rows, columns), dtype=int)
-    
+
     # random tiles in linear array
-    tile = choice(arange(1,ntiles+1, dtype=int), size=ntiles, 
+    tile = choice(arange(1,ntiles+1, dtype=int), size=ntiles,
                   replace=False)
-    
+
     # new tiles in Letter expansion start at 21
     if game == 2:
         ig = where(tile > 16)[0]
@@ -52,17 +52,17 @@ def deploy_random_board(game):
         for c in range(columns):
             i = r*columns + c
             board[r,c] = tile[i]
-    
+
     return board
 
 
 def validate_board_fountain(game,board):
 
-    rows,columns = board.shape 
+    rows,columns = board.shape
 
     fountain = array(where(board==7)).reshape(2)
-    
-    # for great bazaar variant the Fountain is at the center 
+
+    # for great bazaar variant the Fountain is at the center
     # for the rest, it is at any of the center tiles
     if game == 3:
         exchange_tiles(board,(2,2),(fountain[0],fountain[1]))
@@ -78,7 +78,7 @@ def validate_board_blackmarket_teahouse(game,board):
 
     black = array(where(board==8)).reshape(2)
     tea = array(where(board==9)).reshape(2)
-    
+
     # randomize position of tea house until conditions
     # are satisfied
     r,c = tea[0],tea[1]
@@ -92,13 +92,13 @@ def validate_board_blackmarket_teahouse(game,board):
     else:
         while dist < 3 or (r==black[0] or c==black[1]):
             dist = relocate_tea_house(board)
-             
+
     exchange_tiles(board,(r,c),(tea[0],tea[1]))
 
 
 def relocate_tea_house(board):
- 
-    rows,columns = board.shape 
+
+    rows,columns = board.shape
 
     fountain = array(where(board==7)).reshape(2)
     black = array(where(board==8)).reshape(2)
@@ -109,7 +109,7 @@ def relocate_tea_house(board):
 
     # we do not allow Tea House to replace the Fountain
     if r==fountain[0] and c==fountain[1]:
-        return 10
+        return 0
 
     return fabs(black[0]-r) + fabs(black[1]-c)
 
