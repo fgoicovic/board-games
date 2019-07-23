457fce42ffa748e319f472a153aebea3ffb51379
Felipe Goicovic <felipe.goicovic@gmail.com>
2019-07-23 13:30:49 +0200

    Adding option of expansions

--- random_game.py
+++ random_game.py
@@ -8,6 +8,7 @@ from sys import argv,exit
 from magic import from_file
 import sys, os, csv
 
+import datetime
 import argparse
 
 class OptionsParser:
@@ -37,6 +38,13 @@ class OptionsParser:
         self.parser.add_argument("--short",
                                  dest   = "short",
                                  help   = "Select among games of less than 1 hr.",
+                                 action = "store_true"
+                                )
+
+        self.parser.add_argument("--exp, --expansion",
+                                 dest   = "exp",
+                                 help   = "Select basegame with expansion(s) if\n \
+                                           available",
                                  action = "store_true"
                                 )
 
@@ -78,18 +86,20 @@ def query_yes_no(question, default="yes"):
 
 if __name__ == "__main__":
 
+    get_idx = lambda x, xs: [i for (y,i) in zip(xs, range(len(xs))) if x in y]
+
     op = OptionsParser()
     args = op.get_args()
 
     numplayers = args.num
 
     file_name = args.col
-    outname = file_name.replace('.csv', '.txt')
-    outname = '{}pl_'.format(numplayers) + outname
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         row_count = sum(1 for row in csv_reader)
         print('File contains {} lines'.format(row_count))
+
+    outname = 'played_' + file_name
 
     N = row_count - 1 # not counting header
     name = N*['']
@@ -98,8 +108,7 @@ if __name__ == "__main__":
     bstplayers = N*[[0],]
     players = np.zeros((N,2), dtype=np.int8)
     playtime = np.zeros(N, dtype=np.int16)
-    cid = np.zeros(N, dtype=np.int32)
-    get_idx = lambda x, xs: [i for (y,i) in zip(xs, range(len(xs))) if x in y]
+    #cid = np.zeros(N, dtype=np.int32)
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line = 0
@@ -113,14 +122,14 @@ if __name__ == "__main__":
                 irec = get_idx('rec', row[iplayer])
                 ibst = get_idx('best', row[iplayer])
                 itime = get_idx('playingtime', row)
-                iid = get_idx('collid', row)
+                #iid = get_idx('objetctid', row)
             else:
                 name[line-1] = row[0]
                 gtype[line-1] = row[itype[0]]
                 row_pl = row[iplayer]
                 players[line-1, 0] = int(row_pl[imin])
                 players[line-1, 1] = int(row_pl[imax])
-                cid[line-1] = int(row[iid])
+                #cid[line-1] = int(row[iid])
                 playtime[line-1] = int(row[itime[0]])
                 if row_pl[irec][0]:
                     recplayers[line-1] = list(map(int, row_pl[irec[0]].split(',')))
@@ -132,25 +141,36 @@ if __name__ == "__main__":
     name = np.array(name)
     bstplayers = np.array(bstplayers)
     recplayers = np.array(recplayers)
-    idx = (gtype == 'expansion')
-    basegame = name[~idx]
-    expansion = name[idx]
-    players_bg = players[~idx]
-    players_xp = players[idx]
-    bstpl_bg = bstplayers[~idx]
-    recpl_bg = recplayers[~idx]
-    bstpl_xp = bstplayers[idx]
-    time_bg = playtime[~idx]
-    time_xp = playtime[idx]
-    for b,bg in enumerate(basegame):
-        iexp = get_idx(bg, expansion)
 
-    played = []
-    if not args.nosaved:
+    idx = (gtype == 'expansion')
+    basegame, expansion = name[~idx], name[idx]
+    players_bg, players_xp = players[~idx], players[idx]
+    bstpl_bg, bstpl_xp = bstplayers[~idx], bstplayers[idx]
+    recpl_bg, recpl_xp = recplayers[~idx], recplayers[idx]
+    time_bg, time_xp = playtime[~idx], playtime[idx]
+
+    if args.exp:
+        exp_list = []
+        for b,bg in enumerate(basegame):
+            iexp = get_idx(bg, expansion)
+            exp_list.append(expansion[iexp])
+
+    played, num_played, date_played = [], [], []
+    if numplayers > 0 and not args.nosaved:
         if os.path.isfile(outname):
-            f = open(outname, "r")
-            played = [i.replace('\n', '') for i in f.readlines()]
-            f.close()
+            with open(outname) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                line = 0
+                for row in csv_reader:
+                    if line > 0:
+                        played.append(row[0])
+                        num_played.append(int(row[1]))
+                        date_played.append(row[2])
+                    line += 1
+            played = np.array(played)
+            num_played = np.array(num_played)
+            idx = (num_played == numplayers)
+            played = played[idx]
 
     if args.short:
         idx = (time_bg < 60)
@@ -173,13 +193,13 @@ if __name__ == "__main__":
     reco = []
     ib = 0
     for bst, rec, bg in zip(bstpl_bg, recpl_bg, basegame):
-        if numplayers in bst:
+        if numplayers > 0 and numplayers in bst:
             p[ib] = 3.
             best.append(bg)
-        elif numplayers in rec:
+        elif numplayers > 0 and numplayers in rec:
             p[ib] = 1.
             reco.append(bg)
-        else:
+        elif numplayers > 0:
             p[ib] = 0.
         ib += 1
 
@@ -189,6 +209,9 @@ if __name__ == "__main__":
     if len(reco) > 0:
         print(f"\nNumber of players ({numplayers}) is recommended for:")
         print(reco)
+    if numplayers > 0 and len(best)==0 and len(reco)==0:
+        print(f"\nI don't have a game appropiate for {numplayers} players. Exiting")
+        sys.exit()
 
     if len(played) > 0:
         print("")
@@ -198,8 +221,7 @@ if __name__ == "__main__":
                 print(f"It appears you already have played '{bg}'. Skipping it.")
 
     if np.sum(p) == 0.:
-        print("You have played all posible games with this number of"+\
-               " players.")
+        print("You have played all posible games with this number of players.")
         reset = query_yes_no("Do you wish to reset the list of played games?")
         if not reset:
             print("Nothing to do here. Exiting.")
@@ -211,8 +233,25 @@ if __name__ == "__main__":
 
     p /= np.sum(p)
 
-    result = choice(basegame, p=p)
-    print(f"\nI have chosen '{result}' for you to play. Enjoy!\n")
+    indices = np.arange(len(basegame))
+    result = choice(indices, p=p)
+    if args.exp:
+        result_exp = None
+        if len(exp_list[result])>0:
+            ind_exp = choice(np.arange(len(exp_list[result])+1))
+            if ind_exp>0:
+                result_exp = exp_list[result][ind_exp-1]
+        if result_exp != None:
+            print(f"\nI have chosen '{basegame[result]}' with the expansion"+ \
+                   " '{result_exp}'.\n")
+        else:
+            print(f"\nI have chosen '{basegame[result]}'.\n")
+    else:
+        print(f"\nI have chosen '{basegame[result]}'.\n")
+
+    if not os.path.isfile(outname):
+        f = open(outname, "w")
+        f.write('name,players,date\n')
 
     save = query_yes_no("Do you wish to save this game as played?")
 
@@ -220,7 +259,7 @@ if __name__ == "__main__":
         print(f"Saving the game's name in '{outname}'")
 
         f = open(outname, "a+")
-        f.write(f'{result}\n')
+        f.write('{0},{1},{2}\n'.format(result,numplayers,datetime.date.today()))
         f.close()
 
 
